f6ec4a0dc447b283f615a2aa4e1c743a18690b30
John Nguyen <jtnguyen236@gmail.com>
2018-09-18 16:07:11 +0800

    adds image caching and gallery

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
+	"image"
+	"image/jpeg"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"path"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
+	"html/template"
+
+	"github.com/nfnt/resize"
 	"github.com/pierrre/archivefile/zip"
 	"gopkg.in/alecthomas/kingpin.v2"
 
@@ -22,38 +26,16 @@ import (
 )
 
 type Config struct {
-	StoragePath string
-	Port        int
+	StoragePath    string
+	Port           int
+	ImageCachePath string
 }
 
 var (
-	storagePathFlag = kingpin.Flag("storagePath", "Storage path").Required().String()
-	portFlag        = kingpin.Flag("port", "Serve port").Required().Int()
+	imageCachePathFlag = kingpin.Flag("imageCachePath", "Image cache path").Required().String()
+	storagePathFlag    = kingpin.Flag("storagePath", "Storage path").Required().String()
+	portFlag           = kingpin.Flag("port", "Serve port").Required().Int()
 )
-
-const page = `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Photo Upload</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="/static/css/tachyons.min.css">
-	<link rel="stylesheet" href="/static/css/dropzone.css">
-    <script src="/static/js/dropzone.js"></script>
-</head>
-
-<body>
-    <h1>Upload your photos.</h1>
-    <form action="/" class="dropzone">
-        <div class="fallback">
-            <input name="file" type="file" multiple />
-        </div>
-    </form>
-</body>
-
-</html>
-`
 
 // FileServer conveniently sets up a http.FileServer handler to serve
 // static files from a http.FileSystem.
@@ -74,18 +56,75 @@ func FileServer(r chi.Router, path string, root http.FileSystem) {
 		fs.ServeHTTP(w, r)
 	}))
 }
+
+type Files []os.FileInfo
+
+func list(imageCachePath string) ([]string, error) {
+	infos, err := ioutil.ReadDir(imageCachePath)
+	if err != nil {
+		return nil, err
+	}
+
+	sort.Slice(infos, func(i, j int) bool {
+		return infos[i].ModTime().UnixNano() > infos[j].ModTime().UnixNano()
+	})
+
+	urls := []string{}
+	for _, info := range infos {
+		urls = append(urls, "/imagecache/"+info.Name())
+	}
+	return urls, nil
+}
+
+func resizeImage(b []byte) ([]byte, error) {
+	r := bytes.NewReader(b)
+	img, err := jpeg.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	m := resize.Resize(512, 0, img, resize.Lanczos3)
+	buf := &bytes.Buffer{}
+	err = jpeg.Encode(buf, m, nil)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func writeCache(filename string, img image.Image) error {
+	buf := &bytes.Buffer{}
+	err := jpeg.Encode(buf, img, nil)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, buf.Bytes(), os.ModePerm)
+}
+
+func readCache(filename string) ([]byte, error) {
+	return ioutil.ReadFile(filename)
+}
+
 func main() {
 	box := packr.NewBox("static")
+	pages := packr.NewBox("templates")
 	kingpin.Parse()
 	config := &Config{
-		Port:        *portFlag,
-		StoragePath: *storagePathFlag,
+		Port:           *portFlag,
+		StoragePath:    *storagePathFlag,
+		ImageCachePath: *imageCachePathFlag,
 	}
 	r := chi.NewMux()
 	r.Handle("/files/*", http.StripPrefix("/files", http.FileServer(http.Dir(config.StoragePath))))
+	r.Handle("/imagecache/*", http.StripPrefix("/imagecache", http.FileServer(http.Dir(config.ImageCachePath))))
 	r.Handle("/static/*", http.StripPrefix("/static", http.FileServer(box)))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(page))
+		files, err := list(config.ImageCachePath)
+		if err != nil {
+			http.Error(w, "could not list files", 500)
+			return
+		}
+		tpl := template.Must(template.New("index").Parse(pages.String("index.html")))
+		tpl.Execute(w, files)
 	})
 	r.Get("/files/all", func(w http.ResponseWriter, r *http.Request) {
 		tmpDir, err := ioutil.TempDir("", "zip")
@@ -109,7 +148,7 @@ func main() {
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		file, info, err := r.FormFile("file")
 		if err != nil {
-			log.Println("can not open form file:", err)
+			fmt.Println("can not open form file:", err)
 			http.Error(w, "can not open form file: "+err.Error(), 400)
 			return
 		}
@@ -117,29 +156,32 @@ func main() {
 		defer file.Close()
 		ext := filepath.Ext(info.Filename)
 
-		// Create a file with the same name
-		// assuming that you have a folder named 'uploads'
-		fmt.Println(time.Now().Format(time.RFC3339Nano) + ext)
-		out, err := os.OpenFile(
-			path.Join(config.StoragePath, time.Now().Format("2006-01-02T15-04-05.999999999")+ext),
-			os.O_WRONLY|os.O_CREATE, 0666,
-		)
-
+		b, err := ioutil.ReadAll(file)
 		if err != nil {
-			log.Println("could not create file:", err)
-			http.Error(w, "could not create file: "+err.Error(), 500)
+			fmt.Println("could not read file:", err)
+			http.Error(w, "could not read file: "+err.Error(), 500)
 			return
 		}
-		defer out.Close()
-
-		_, err = io.Copy(out, file)
+		err = ioutil.WriteFile(filepath.Join(config.StoragePath, time.Now().Format("2006-01-02T15-04-05.999999999")+ext), b, os.ModePerm)
 		if err != nil {
-			log.Println("could not write file:", err)
+			fmt.Println("could not write file:", err)
+			http.Error(w, "could not write file: "+err.Error(), 500)
+			return
+		}
+
+		resized, err := resizeImage(b)
+		if err != nil {
+			http.Error(w, "could not resize jpeg: "+err.Error(), 500)
+			return
+		}
+		err = ioutil.WriteFile(filepath.Join(config.ImageCachePath, time.Now().Format("2006-01-02T15-04-05.999999999")+ext), resized, os.ModePerm)
+		if err != nil {
+			fmt.Println("could not write file:", err)
 			http.Error(w, "could not write file: "+err.Error(), 500)
 			return
 		}
 	})
 
-	log.Println("Serving on:", config.Port)
+	fmt.Println("Serving on:", config.Port)
 	log.Fatalln(http.ListenAndServe(":"+strconv.Itoa(config.Port), r))
 }
